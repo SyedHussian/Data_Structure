da89f2529ac9d3ba65088d2a85e60aa67e263af8
SyedHussain <tah94mid@gmail.com>
2020-01-26 16:00:22 -0500

    Insertion sort

--- src/sorting/Sort.java
+++ src/sorting/Sort.java
@@ -3,9 +3,8 @@ package sorting;
 public class Sort {
 	
 	public void bubbleSort(int arr[]) {
-		int n = arr.length;
-		for (int i=0; i<n-1; i++) {
-			for (int j=0; j<n-i-1; j++) {
+		for (int i=0; i<arr.length-1; i++) {
+			for (int j=0; j<arr.length-i-1; j++) {
 				if (arr[j]>arr[j+1]) {
 					int temp = arr[j];
 					arr[j] = arr[j+1];
@@ -15,9 +14,37 @@ public class Sort {
 		}
 	}
 	
+	public void seletionSort(int arr[]) {
+		for (int i=0; i<arr.length; i++) {
+			int minIndex = i;
+			for (int j = i+1; j<arr.length; j++) {
+				if (arr[j] < arr[minIndex]) {
+					minIndex = j;
+				}
+			}
+			if (minIndex != i) {
+				int temp = arr[i];
+				arr[i] = arr[minIndex];
+				arr[minIndex] = temp;
+			}
+		}
+	}
+	
+	public void insertionSort(int arr[]) {
+		for (int i=1; i<arr.length; i++) {
+			int temp = arr[i], j=i;
+			while (j>0 && arr[j-1]>temp) {
+				arr[j]=arr[j-1];
+				j--;
+			}
+			arr[j]=temp;
+		}
+	}
+	
+	
+	
 	public void printArray(int arr[]) {
-		int n = arr.length;
-		for (int i=0; i<n; i++) {
+		for (int i=0; i<arr.length; i++) {
 			System.out.println(arr[i]+" ");
 		}
 		System.out.println();
--- src/sorting/SortMain.java
+++ src/sorting/SortMain.java
@@ -4,15 +4,52 @@ public class SortMain {
 	
 	public static void main(String[] args) {
 		
-		Sort bSort = new Sort();
-		int[] arr = { 10, 5, 30, 15, 50, 6 };
-		System.out.println("Array to be sorted...");
-		bSort.printArray(arr);
+		final long start1 = System.nanoTime();
+		
+		System.out.println("Bubble Sort........");
+		Sort sort = new Sort();
+		int[] bArr = { 10, 5, 30, 15, 50, 6 };
+		System.out.println("Array to be sorted using Bubble sort...");
+		sort.printArray(bArr);
 
-		bSort.bubbleSort(arr);
+		
+		System.out.println("Sorted Array using Bubble sort...");
+		sort.bubbleSort(bArr);
+		sort.printArray(bArr);
+		
+		final long duration1 = System.nanoTime() - start1;
+		System.out.println("Time taken in nano seconds: "+ duration1);
+		System.out.println("-----------------------------");
+		
+		final long start2 = System.nanoTime();
+		System.out.println("Selection Sort........");
+		int[] sArr = { 10, 5, 30, 15, 50, 6 };
+		
+		System.out.println("Array to be sorted using Selection sort...");
+		sort.printArray(sArr);
 
-		System.out.println("Sorted Array...");
-		bSort.printArray(arr);
+		System.out.println("Sorted Array using Selection sort...");
+		sort.seletionSort(sArr);
+		sort.printArray(sArr);
+		
+		final long duration2 = System.nanoTime() - start2;
+		System.out.println("Time taken in nano seconds: "+ duration2);
+		System.out.println("-----------------------------");
+		
+		final long start3 = System.nanoTime();
+		System.out.println("Insertion Sort........");
+		int[] iArr = { 10, 5, 30, 15, 50, 6 };
+		
+		System.out.println("Array to be sorted using Insertion sort...");
+		sort.printArray(iArr);
+
+		System.out.println("Sorted Array using Insertion sort...");
+		sort.insertionSort(iArr);
+		sort.printArray(iArr);
+		
+		final long duration3 = System.nanoTime() - start3;
+		System.out.println("Time taken in nano seconds: "+ duration3);
+		System.out.println("-----------------------------");
 
 	}
 	
