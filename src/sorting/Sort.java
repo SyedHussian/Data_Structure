68a26c3f6e7bbfbf82c0900113ba3b90cdd38ccd
SyedHussain <tah94mid@gmail.com>
2020-01-30 12:42:02 -0500

    bucket sort

--- src/sorting/Sort.java
+++ src/sorting/Sort.java
@@ -1,7 +1,14 @@
 package sorting;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Sort {
+	int arr[];
 	
+	public Sort(int[] arr) { this.arr = arr;}
+	public Sort() { }
+
 	public void bubbleSort(int arr[]) {
 		for (int i=0; i<arr.length-1; i++) {
 			for (int j=0; j<arr.length-i-1; j++) {
@@ -42,6 +49,69 @@ public class Sort {
 	}
 	
 	
+	public void bucketSort(int arr[]) {
+		// creating sqrt# of buckets, se that the distribution is even 
+		int numberOfBuckets = (int) Math.ceil(Math.sqrt(arr.length));
+		int maxValue = Integer.MIN_VALUE;
+		int minValue = Integer.MAX_VALUE;
+		
+		
+		//Finding the min and max value from the array
+		for (int value: arr) {
+			if (value< minValue) {
+				minValue = value;
+			}
+			else if (value > maxValue) {
+				maxValue = value;
+			}
+		}
+		
+		//Create an array of buckets
+		ArrayList<Integer>[] buckets = new ArrayList[numberOfBuckets];
+		
+		
+		//initializing empty bucket
+		for (int i=0; i<buckets.length; i++) {
+			buckets[i] = new ArrayList<Integer>();
+		}
+		
+		for (int value: arr) {
+			int bucketNumber = (int) Math.ceil((value * numberOfBuckets)/maxValue);
+			//System.out.println("bucketNumber: " + bucketNumber);
+			buckets[bucketNumber - 1].add(value);
+		}
+		
+		System.out.println("\n\nPrinting buckets before Sorting...");
+		printBucket(buckets);
+		
+		for (ArrayList<Integer> bucket : buckets) {
+			Collections.sort(bucket);
+		}
+		
+		System.out.println("\n\nPrinting buckets after sorting!");
+		printBucket(buckets);
+		
+		int index = 0;
+		for (ArrayList<Integer> bucket: buckets) {
+			for (int value: bucket) {
+				arr[index] = value;
+				index++;
+			}
+		}
+		
+		
+	}
+	
+	//Prints Buckets
+	public void printBucket(ArrayList<Integer>[] buckets) {
+		for(int i=0; i<buckets.length; i++) {
+			System.out.println("\nBucket#" + i + " :");
+			for (int j=0; j<buckets[i].size(); j++) {
+				System.out.print(buckets[i].get(j)+"  ");
+			}
+		}
+		
+	}
 	
 	public void printArray(int arr[]) {
 		for (int i=0; i<arr.length; i++) {
--- src/sorting/SortMain.java
+++ src/sorting/SortMain.java
@@ -1,55 +1,78 @@
 package sorting;
 
+import java.util.Random;
+
 public class SortMain {
 	
 	public static void main(String[] args) {
 		
-		final long start1 = System.nanoTime();
+//		final long start1 = System.nanoTime();
+//		
+//		System.out.println("Bubble Sort........");
+//		Sort sort = new Sort();
+//		int[] bArr = { 10, 5, 30, 15, 50, 6 };
+//		System.out.println("Array to be sorted using Bubble sort...");
+//		sort.printArray(bArr);
+//
+//		
+//		System.out.println("Sorted Array using Bubble sort...");
+//		sort.bubbleSort(bArr);
+//		sort.printArray(bArr);
+//		
+//		final long duration1 = System.nanoTime() - start1;
+//		System.out.println("Time taken in nano seconds: "+ duration1);
+//		System.out.println("-----------------------------");
+//		
+//		final long start2 = System.nanoTime();
+//		System.out.println("Selection Sort........");
+//		int[] sArr = { 10, 5, 30, 15, 50, 6 };
+//		
+//		System.out.println("Array to be sorted using Selection sort...");
+//		sort.printArray(sArr);
+//
+//		System.out.println("Sorted Array using Selection sort...");
+//		sort.seletionSort(sArr);
+//		sort.printArray(sArr);
+//		
+//		final long duration2 = System.nanoTime() - start2;
+//		System.out.println("Time taken in nano seconds: "+ duration2);
+//		System.out.println("-----------------------------");
+//		
+//		final long start3 = System.nanoTime();
+//		System.out.println("Insertion Sort........");
+//		int[] iArr = { 10, 5, 30, 15, 50, 6 };
+//		
+//		System.out.println("Array to be sorted using Insertion sort...");
+//		sort.printArray(iArr);
+//
+//		System.out.println("Sorted Array using Insertion sort...");
+//		sort.insertionSort(iArr);
+//		sort.printArray(iArr);
+//		
+//		final long duration3 = System.nanoTime() - start3;
+//		System.out.println("Time taken in nano seconds: "+ duration3);
+//		System.out.println("-----------------------------");
+//		
 		
-		System.out.println("Bubble Sort........");
-		Sort sort = new Sort();
-		int[] bArr = { 10, 5, 30, 15, 50, 6 };
-		System.out.println("Array to be sorted using Bubble sort...");
-		sort.printArray(bArr);
-
+		//--------------------------- Bucket Sorting ----------------//
+		int arr[] = new int[100];
 		
-		System.out.println("Sorted Array using Bubble sort...");
-		sort.bubbleSort(bArr);
-		sort.printArray(bArr);
+		//Generating 100 random numbers in the range of 0-100
+		Random random = new Random();
+		for(int i=0;i<100;i++) {
+			arr[i] = random.nextInt(100)+100;
+		}
 		
-		final long duration1 = System.nanoTime() - start1;
-		System.out.println("Time taken in nano seconds: "+ duration1);
-		System.out.println("-----------------------------");
 		
-		final long start2 = System.nanoTime();
-		System.out.println("Selection Sort........");
-		int[] sArr = { 10, 5, 30, 15, 50, 6 };
+		//Passing this array to BucketSort method
+		Sort bs = new Sort(arr);
+		System.out.println("Array before Sorting: ");
+		bs.printArray(arr);
+		bs.bucketSort(arr);
 		
-		System.out.println("Array to be sorted using Selection sort...");
-		sort.printArray(sArr);
-
-		System.out.println("Sorted Array using Selection sort...");
-		sort.seletionSort(sArr);
-		sort.printArray(sArr);
 		
-		final long duration2 = System.nanoTime() - start2;
-		System.out.println("Time taken in nano seconds: "+ duration2);
-		System.out.println("-----------------------------");
-		
-		final long start3 = System.nanoTime();
-		System.out.println("Insertion Sort........");
-		int[] iArr = { 10, 5, 30, 15, 50, 6 };
-		
-		System.out.println("Array to be sorted using Insertion sort...");
-		sort.printArray(iArr);
-
-		System.out.println("Sorted Array using Insertion sort...");
-		sort.insertionSort(iArr);
-		sort.printArray(iArr);
-		
-		final long duration3 = System.nanoTime() - start3;
-		System.out.println("Time taken in nano seconds: "+ duration3);
-		System.out.println("-----------------------------");
+		System.out.println("\n\nArray after Sorting: ");
+		bs.printArray(arr);
 
 	}
 	
